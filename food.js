b4ea5c4f8385648a43a6a0e9dccb6a03257c7bad
Pr4santhi <73959648+Pr4santhi@users.noreply.github.com>
2021-03-18 19:07:40 +0000

    Add files via upload

---
+++ food.js
@@ -0,0 +1,24 @@
+class food{
+
+    constructor(){
+        this.image = loadImage("images/Milk.png");
+    }
+
+    display(){
+        var x=80,y=100;
+
+        imageMode(CENTER);
+        image(this.image,720,220,70,70);
+
+        if (this.food!=0) {
+         for(var i=0;i<this.foodstock;i++){
+             if(i%10==0){
+                 x=80;
+                 y=y+50;
+             }
+             image(this.image,x,y,50,50);
+             x=x+30;
+         }   
+        }
+    }
+}
